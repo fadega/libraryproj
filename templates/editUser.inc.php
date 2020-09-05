944b1c4be0d46f9550a6ca023615582ceb8d2e8c
Fadega <fadega.araya@gmail.com>
2020-09-05 21:08:11 +1000

    A feature to edit user details has been added

---
+++ templates/editUser.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+
+if(isset($_POST['firstname'])&& isset($_POST['lastname']) && isset($_POST['email'])){
+        // require '../templates/libcommon.php';
+        require '../app/dbh.php';
+        $firstname  =    $_POST['firstname'];
+        $lastname   =    $_POST['lastname'];
+        $email      =    $_POST['email'];
+        $id         =    $_POST['idupdate'];
+
+
+        $details = [
+            'fname' => $firstname,
+            'lname' => $lastname,
+            'emailId' => $email,
+            'uid' => $id
+          ];
+
+            try{
+              $sql = "UPDATE user SET firstname=:fname, lastname=:lname, email=:emailId WHERE user_id=:uid";
+              $stmt= $conn->prepare($sql);
+              $stmt->execute($details);
+              $count = $stmt->rowCount();
+              if($count == 0){
+                  $status = false;
+              }
+              else{
+                  $status= true;
+              }
+              if($status){
+                  header('Location:../templates/index.php?update=success&count='.$count);
+                  exit();
+              }else{
+                header('Location:../templates/index.php?update=failed&count='.$count);
+                exit();
+              }
+
+            }catch(PDOException $e){
+                // header("Location:../templates/index.php?update=failed&errType=".$e->getMessage());
+                // exit();
+            }?>
+            <div class="">
+              <?php
+              echo $status.'<br><pre>';
+              print_r($_POST); ?>
+
+            </div>
+            <?php
+
+}else{
+  header('Location:../templates/index.php?error=illegal-update-attempt');
+  exit();
+}
+ ?>
---
+++ templates/editUser.php
@@ -0,0 +1,44 @@
+
+<?php  require '../app/header.php';
+    require '../app/dbh.php';
+  // require '../templates/libcommon.php';
+    $id= $_GET['id'];
+
+   //To push data to the textfields from
+    $sql = 'SELECT *FROM user WHERE user_id=:id';
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([':id'=>$id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+
+<main>
+        <h1>Update details</h1>
+
+        <!-- <form name="my-form" action="../templates/editUser.inc.php" method="post"> -->
+        <form name="my-form" action ="../templates/editUser.inc.php" method="post">
+            <div class="form-box">
+                <label for="fname">First name</label>
+                <input type="text" id="firstname" name="firstname" value="<?php echo $result['firstname'] ;?>" >
+            </div>
+
+            <div class="form-box">
+                <label for="lname">Last name</label>
+                <input type="text" id="lastname" name="lastname" value="<?php echo $result['lastname'] ;?>">
+            </div>
+
+            <div class="form-box">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" value="<?php echo $result['email'] ;?>">
+            </div>
+            <div class="form-box">
+                <button type="submit" id="btnSend" name="idupdate" value="<?php echo $id;?>">Update</button>
+            </div>
+
+
+        </form>
+    </main>
+
+
+
+<?php require '../app/footer.php';?>
